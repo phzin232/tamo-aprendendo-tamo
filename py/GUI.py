69eb77ecb434387962c265bd43c1c613842a5b95
phzin232 <ph.pdro123@gmail.com>
2026-01-21 15:17:54 -0300

    gui

--- py/GUI.py
+++ py/GUI.py
@@ -1,14 +1,17 @@
-import tkinter as tk
-from tkinter import ttk
+import customtkinter as ctk
+
+ctk.set_appearance_mode("dark")
+ctk.set_default_color_theme("dark-blue")
 
 def main():
     app = Application()
     app.mainloop()
 
-class Application(tk.Tk):
+class Application(ctk.CTk):
     def __init__(self, ):
         super().__init__()
         self.title("Monitoramento de notas")
+        self.geometry("800x400")
 
 
         self.columnconfigure(0, weight=1)
@@ -20,10 +23,9 @@ class Application(tk.Tk):
 
         frame2 = InputForm(self)
         frame2.grid(row = 0, column=1, sticky="nsew",padx=5, pady=5)
-        self.configure(bg="#1e1e1e")
 
 
-class InputForm(ttk.Frame):
+class InputForm(ctk.CTkFrame):
     def __init__(self, parent):
         super().__init__(parent)
 
@@ -31,28 +33,34 @@ class InputForm(ttk.Frame):
         self.rowconfigure(1, weight=1)
         
 
-        self.entry = ttk.Entry(self)
+        self.entry = ctk.CTkEntry(self)
         self.entry.grid(row=0, column=0,sticky="ew")
 
         self.entry.bind("<Return>", lambda event: self.add_to_list())
 
-        self.entry_bnt = ttk.Button(self, text="add",command=self.add_to_list)
+        self.entry_bnt = ctk.CTkButton(self, text="add",command=self.add_to_list)
         self.entry_bnt.grid(row=0, column=1)
 
-        self.entry_bnt = ttk.Button(self, text="Clear",command=self.clear_list)
+        self.entry_bnt = ctk.CTkButton(self, text="Clear",command=self.clear_list)
         self.entry_bnt.grid(row=0, column=2)
 
-        self.text_list = tk.Listbox(self)
+        self.text_list = ctk.CTkTextbox(self, height=200)
         self.text_list.grid(row=1, column=0, columnspan=2, sticky="nsew")
-        self.configure(style="Dark.TFrame")
+        self.listbox = ctk.CTkTextbox(self, height=200)
+        self.listbox.configure(state ="disabled")
 
     def add_to_list(self):
         text =  self.entry.get()
         if text:
-            self.text_list.insert(tk.END, text)
-            self.entry.delete(0, tk.END)
+            self.text_list.insert("end", text + "\n")
+            self.listbox.configure(state ="normal") 
+            self.listbox.configure(state ="disabled")
+            self.entry.delete(0, "end")
+
     def clear_list(self):
-        self.text_list.delete(0, tk.END)
+        self.listbox.configure(state ="normal")  # Enable editing
+        self.text_list.delete("1.0", "end")
+        self.listbox.configure(state ="disabled")  # Disable editing
 
 
 if __name__ == "__main__":
